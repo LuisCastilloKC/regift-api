6c1c880d21ab7b497770afb35f03ec8fd648e489
Luis Castillo <mr.luiscastillo@gmail.com>
2022-07-13 21:31:59 -0400

    add all routes for gifts

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   post "/login", to: "auth#login"
   get "/auto_login", to: "auth#auto_login"
   get "/user_is_authed", to: "auth#user_is_authed"
+
+  resources :gits
 end
