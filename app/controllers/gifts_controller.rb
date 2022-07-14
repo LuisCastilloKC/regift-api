c07a9c45240018a6cb750b49d5fe5f1f288d26f2
Luis Castillo <mr.luiscastillo@gmail.com>
2022-07-13 21:15:51 -0400

    add create gift method controller

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -5,4 +5,14 @@ class GiftsController < ApplicationController
         render json: gift
     end
 
+    def create
+        gift = Gift.create(gift_params)
+
+        if gift.save
+            render json: gift, status: :created
+        else
+            render json: gift.errors, status: :unprocessable_entity
+        end
+    end
+
 end
